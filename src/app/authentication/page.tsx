3fe8c2a56312be208377caffc3032b4bcd73bf1d
Rafael de Lima <rafalimabcl@gmail.com>
2025-06-02 13:19:59 -0300

    feat: add sing up form

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,25 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { LoginForm } from "./components/login-form";
+import { SignUpForm } from "./components/sign-up-form";
+
+const AuthenticationPage = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Criar conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="register">
+          <SignUpForm />
+        </TabsContent>
+        <TabsContent value="login">
+          <LoginForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default AuthenticationPage;
